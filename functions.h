0604c7cd001d3f7c35a1c340bfba6c9bfde6f547
Oleksandr Liashenko <99184219+LiashenkOL@users.noreply.github.com>
2022-02-21 11:46:12 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,29 +1,5 @@
 #pragma once
-#include <iostream>
-using namespace std;
 
-float findMinValue(float n1, float n2, float n3)
-{
-	float min = 0.0f;
-	
-	//TODO: write your code here
-	
-	float num[3];
-	num[0] = n1;
-	num[1] = n2;
-	num[2] = n3;
 
-	min = num[0];
 
-	for (int i = 0; i < 3; ++i)
-	{
-		if (num[i] < min)
-		{
-			min = num[i];
-		}
-
-	}
-
-	cout << "Min is: " << min << endl;
-	return min;
-}
+float findMinValue(float n1, float n2, float n3);
