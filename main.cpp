aad548c7a897b137a59714950a5f2bd0e1220fb1
Oleksandr Liashenko <99184219+LiashenkOL@users.noreply.github.com>
2022-02-21 10:53:43 +0200

    Liashenko

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 int main()
 {
 	//TODO: if you want to test your functions, write your code here
-	
-	
+	float n1, n2, n3;
+	cout << "Enter Number 1, 2, 3: ";
+	cin >> n1 >> n2 >> n3;
+	findMinValue(n1, n2, n3);
 	
 	return 0;
 }
